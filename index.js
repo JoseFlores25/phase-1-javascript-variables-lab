9ce5ab21a84f7ca26dd9dc742bd0763f5ec3a921
Jose <flores.jose2185@gmail.com>
2021-11-01 12:58:34 -0400

    Work is finish

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const companyName = "Scuber";
+
+function Scuber() {
+  expect(Scuber).to.equal("Scuber");
+}
+
+let mostProfitableNeighborhood = "Chelsea";
+
+function mostProfitableNeighborhood() {
+  expect(Chelsea).to.equal("Chelsea");
+}
+
+let companyCeo = "Susan Smith";
